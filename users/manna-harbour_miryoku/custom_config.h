0d2e7688b0f693accf9018dbaa6ca8ada94fa51b
Hanif Norman <hanifnorman@gmail.com>
2022-12-01 21:11:51 +0800

    Make the extra outer keys available for all layers

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -31,20 +31,6 @@ U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U
         N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39\
     )\
     LAYOUT(\
-    XXX, XXX, XXX, XXX, XXX, XXX,                XXX, XXX, XXX, XXX, XXX, XXX,\
-    XXX, K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, XXX,\
-    XXX, K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, XXX,\
-    XXX, K20, K21, K22, K23, K24, XXX,      XXX, K25, K26, K27, K28, K29, XXX,\
-                   XXX, K32, K33, K34,      K35, K36, K37, XXX\
-    )
-    // base layer
-    #define MIRYOKU_LAYERMAPPING_BASE(\
-        K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09,\
-        K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
-        K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
-        N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39\
-    )\
-    LAYOUT(\
     KC_EQL,  KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,\
     KC_GRV,  K00,  K01,  K02,  K03,  K04,                                K05,  K06,  K07,  K08,  K09,  KC_BSLS,\
     KC_LCTL, K10,  K11,  K12,  K13,  K14,                                K15,  K16,  K17,  K18,  K19,  KC_QUOT,\
